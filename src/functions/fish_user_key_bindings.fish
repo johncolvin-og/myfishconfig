f284509a919960dfbdc4363b9bddf5afab7d92aa
John Colvin <john.colvin@bluetradesys.com>
2021-03-11 12:37:20 -0600

    Remove duplicate code in fish_user_key_bindings
    These lines were (and are) in config.fish as well

--- src/functions/fish_user_key_bindings.fish
+++ src/functions/fish_user_key_bindings.fish
@@ -9,8 +9,6 @@ set -x LESS "$LESS -R -Q"
 set -x LESSOPEN "| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
 
 set -x XDG_CONFIG_HOME $HOME"/.config"
-set -g theme_powerline_fonts no
-set -g theme_nerd_fonts yes
 fish_vi_key_bindings
 #set fish_color_cwd A25AE2
 set fish_color_cwd B27AFF
@@ -25,7 +23,6 @@ bind -M insert \cl forward-char
 bind -M insert \ch backward-char
 bind -M insert \cH backward-bigword
 set -x LS_COLORS "$LS_COLORS:di=38;5;141;48;5;234:fi=94:*.h=38;5;039:*.cpp=38;5;024:*.py=38;5;041:*.txt=38;5;251"
-set PATH $PATH /home/john/.tools/
 bind -M default \cv 'set fish_bind_mode default'
 bind -M default \e 'set fish_bind_mode default'
 
