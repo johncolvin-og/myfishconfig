7bcbb43748f489ed69573ef977cf2059813b9769
John Colvin <john.colvin@bluetradesys.com>
2021-04-15 11:31:08 -0400

    refactor: Hide cat-range private impl from shell

--- src/functions/cat-range.fish
+++ src/functions/cat-range.fish
@@ -1,14 +1,13 @@
 #!/usr/bin/env fish
 
-function __cat-range
-    sed -n $argv[1],$argv[2]"p;"(math $argv[2] + 1)"q" $argv[3]
-end
-
-function cat-range -d 'Prints a particular range of lines'
+function cat-range
+    function impl
+        sed -n $argv[1],$argv[2]"p;"(math $argv[2] + 1)"q" $argv[3]
+    end
     set parts (string split ':' $argv[1])
     if test (count $parts) = 2
-        set last_ln (math $parts[1] + $parts[2])
-        __cat-range $parts[1] $last_ln $argv[2]
+        set last_ln (math $parts[1] + $parts[2] - 1)
+        impl $parts[1] $last_ln $argv[2]
         return 0
     end
     if test (count $parts) -gt 2
@@ -17,7 +16,7 @@ function cat-range -d 'Prints a particular range of lines'
     end
     set parts (string split '-' $argv[1])
     if test (count $parts) = 2
-        __cat-range $parts $argv[2]
+        impl $parts $argv[2]
     end
 end
 
