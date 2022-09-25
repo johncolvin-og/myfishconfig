c2e62df2afed8494fd81a368089939c39fdd1e27
johncolvin-og <46972520+johncolvin-og@users.noreply.github.com>
2022-09-25 12:50:57 -0500

    Define colors in their own file
    Include the original names I came up with (some silly) and web names (of nearest
    web color)

---
+++ src/colors/john_colors.fish
@@ -0,0 +1,96 @@
+# Custom colors (closest web names)
+set -U COLOR_raisin_black 222222
+set -U COLOR_dark_charcoal 333333
+set -U COLOR_outer_space 444444
+set -U COLOR_davys_gray 555555
+set -U COLOR_granite_gray 666666
+set -U COLOR_spanish_gray 999999
+set -U COLOR_spanish_gray_l1 9E9E9E
+set -U COLOR_dark_gray A8A8A8
+set -U COLOR_light_silver D8D8D8
+
+set -U COLOR_bitter_lemon CCCC00
+set -U COLOR_dark_yellow 929222
+set -U COLOR_light_gold B2B200
+set -U COLOR_brass AA9944
+
+set -U COLOR_mint_green 99FFAA
+set -U COLOR_apple 55AA55
+set -U COLOR_may_green 429542
+set -U COLOR_light_green 88FF88
+set -U COLOR_celadon_green 228888
+
+set -U COLOR_blueberry 5084FF
+set -U COLOR_french_sky_blue 77B1FF
+set -U COLOR_blueberry_l1 5787FF
+set -U COLOR_lavender B27AFF
+set -U COLOR_lavender_indigo A25AE2
+
+set -U COLOR_rusty_red DD2244
+set -U COLOR_orange FF8822
+set -U COLOR_princeton_orange AA7744
+set -U COLOR_brown AA5744
+set -U COLOR_philippine_gold BB6C00
+set -U COLOR_rajah FFAA55
+
+set -U COLOR_metallic_sunburst 996C33
+set -U COLOR_cosmic_cobalt 223388
+set -U COLOR_field_drab 795C26
+set -U COLOR_coyote_brown 7E6036
+set -U COLOR_metallic_sunburst_l1 A28238
+set -U COLOR_metallic_sunburst_d1 907036
+set -U COLOR_coyote_brown_l1 80602B
+set -U COLOR_burnished_brown A27070
+
+
+# Custom colors (my names)
+set -U my_dark_gray 222222
+set -U my_dark_gray2 333333
+set -U my_semi_dark_gray 444444
+set -U my_semi_dark_gray2 555555
+set -U my_gray 666666
+set -U my_semi_light_gray 999999
+set -U my_semi_light_gray2 9E9E9E
+set -U my_light_gray A8A8A8
+set -U my_off_white D8D8D8
+
+set -U my_pastel_yellow CCCC00
+set -U my_dirty_mustard_yellow 929222
+set -U my_hotdog_mustard_yellow B2B200
+set -U my_off_yellow AA9944
+
+set -U my_pale_green 99FFAA
+set -U my_pea_green 55AA55
+set -U my_dark_pea_green 429542
+set -U my_granny_smith_green 88FF88
+set -U my_turqoise 228888
+
+set -U my_pastel_blue 5084FF
+set -U my_baby_blue 77B1FF
+set -U my_slate_blue 5787FF
+set -U my_pastel_purple B27AFF
+set -U my_semi_light_purple B27AFF
+set -U my_semi_light_orchid A25AE2
+
+set -U my_crimson DD2244
+set -U my_orange FF8822
+set -U my_dirty_orange AA7744
+set -U my_dark_peach AA5744
+set -U my_dark_oranage BB6C00
+set -U my_gold FFAA55
+
+set -U my_olive_tan 996C33
+set -U my_deep_royal_blue 223388
+set -U my_mud_brown 795C26
+set -U my_dry_mud_brown 7E6036
+set -U my_yellow_tan A28238
+set -U my_tan 907036
+set -U my_dark_tan 80602B
+set -U my_orange_tan 907036
+set -U my_light_tan A27070
+
+# colors (new, without corresponding silly names by me..yet)
+set -U COLOR_screamin_green FF72FF
+set -U COLOR_best_sky_blue 4E9FE5
+set -U COLOR_bright_sky_blue 77C4FE
+set -U COLOR_rich_sky_blue 528CD3
